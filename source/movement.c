bdc98b854710c9fd50577f46f4f4d6c69f82136b
mikilauda95 <michele.simili95@gmail.com>
2017-12-08 01:11:17 +0100

    trace movement performed, still needed to calibrate the space computation

--- source/movement.c
+++ source/movement.c
@@ -3,111 +3,269 @@
 #include <unistd.h>
 #include "ev3.h"
 #include "ev3_tacho.h"
+#include "ev3_sensor.h"
 #include "messages.h"
 #include <pthread.h>
+#include <math.h>
+#include <signal.h>
 
 #define LEFT_MOTOR_PORT 66
 #define RIGHT_MOTOR_PORT 67
 #define MAX_SPEED 1050
+#define TIME_STEP 10//milliseconds
+//old value
+/*#define degree_to_lin 260/90*/
+/*#define degree_to_lin 2.08*/
+/*#define degree_to_lin 1.98*/
+#define degree_to_lin 2.10
+/*#define ANGULAR_SPEED 50*/
+/*#define ANGULAR_SPEED 100*/
+#define ANGULAR_SPEED 150
+
 
 #define Sleep( msec ) usleep(( msec ) * 1000 )
 
+enum name {L, R};
+uint8_t motor[2];
+uint8_t compass_sn;
+float compass_value, ang_off;
 
-uint8_t left_motor_sn;
-uint8_t right_motor_sn;
+/*uint8_t motor[0];*/
+/*uint8_t motor[1];*/
 pthread_t movid;
+volatile int pos_flag;
+pthread_mutex_t stopMutex;
+
+double llin_speed, rlin_speed;
+double rpos, lpos, distance=0;
+double angle;
+/*double coord[0], coord[1];*/
+/*double coord[2];*/
+struct coord {
+	float x;
+	float y;
+} coord;
 
 int movement_init(){
-    ev3_tacho_init();
+	ev3_tacho_init();
+	ev3_sensor_init();
 
-    ev3_search_tacho_plugged_in(LEFT_MOTOR_PORT, 0, &left_motor_sn, 0 );
-    ev3_search_tacho_plugged_in(RIGHT_MOTOR_PORT, 0, &right_motor_sn, 0 );
+	ev3_search_tacho_plugged_in(LEFT_MOTOR_PORT, 0, &motor[L], 0 );
+	ev3_search_tacho_plugged_in(RIGHT_MOTOR_PORT, 0, &motor[R], 0 );
 
-    /* Decide how the motors should behave when stopping.
-    We have the alternatives COAST, BRAKE, and HOLD. They result in harder/softer breaking */
-	//speed in cm per secondset_tacho_stop_action_inx( left_motor_sn, TACHO_BRAKE );
-    set_tacho_stop_action_inx( right_motor_sn, TACHO_BRAKE );
+	/* Decide how the motors should behave when stopping.
+	   We have the alternatives COAST, BRAKE, and HOLD. They result in harder/softer breaking */
+	//speed in cm per second
+	set_tacho_stop_action_inx( motor[L], TACHO_BRAKE );
+	set_tacho_stop_action_inx( motor[R], TACHO_BRAKE );
 
-    set_tacho_speed_sp(left_motor_sn, MAX_SPEED  / 5 );
-    set_tacho_speed_sp(right_motor_sn, MAX_SPEED / 5 );
+	set_tacho_speed_sp(motor[L], MAX_SPEED  / 2 );
+	set_tacho_speed_sp(motor[R], MAX_SPEED / 2 );
 
-    return 0;
+	int rets=ev3_search_sensor( HT_NXT_COMPASS, &compass_sn, 0 );
+	if (rets) {
+		printf("FOUND IT\n");
+	}
+	else {
+		printf("NOT FOUND\n");
+	}
+	Sleep(1000);
+	float a=0;
+	for (int i = 0; i < 10; ++i) {
+		get_sensor_value0(compass_sn, &ang_off);
+		a+=ang_off;
+		Sleep(100);
+	}
+	ang_off=a/10;
+
+	printf("OFFSET VALUE IS %f\n",ang_off);
+	Sleep(1000);
+	return 0;
 }
+
+static void _run_to_rel_pos( int l_speed, int l_pos, int r_speed, int r_pos )
+{
+	set_tacho_speed_sp( motor[L], l_speed );
+	set_tacho_speed_sp( motor[R], r_speed );
+	set_tacho_position_sp( motor[L], l_pos );
+	set_tacho_position_sp( motor[R], r_pos );
+	multi_set_tacho_command_inx( motor, TACHO_RUN_TO_REL_POS );
+}
+
+
+
+void TURN_DEGREES( int ang_speed, double angle )
+{
+	/*_run_to_rel_pos( ang_speed, -angle*degree_to_lin, ang_speed, angle*degree_to_lin);*/
+	_run_to_rel_pos( ang_speed, angle*degree_to_lin, ang_speed, -angle*degree_to_lin);
+	Sleep(1000);
+	/*_wait_stopped = 1;*/ //dunno what this refers to so I keep it
+}
+
+void TURN_DEGREES_COMP(int ang_speed, int angle)
+{
+	int i=0;
+	float dest;
+	float buffer[4];
+	int j=0;
+	float avg=0;
+	for (i = 0; i < 4; ++i) {
+		get_sensor_value0(compass_sn,&buffer[i]);
+		/*printf("COMPASS VALUE before TURN : %f\n", buffer[i]);*/
+	}
+	for (i = 0; i < 4; ++i) {
+		avg+=buffer[i];
+	}
+	avg=avg/4;
+	dest=(avg+angle);
+	if (dest>360) {
+		dest-=360;
+	}
+	/*printf("Destination angle is %f and starting is %f\n",dest, compass_value);*/
+	if (angle>0) {
+		set_tacho_speed_sp( motor[L], ang_speed );
+		set_tacho_speed_sp( motor[R], -ang_speed );
+		multi_set_tacho_command_inx(motor, TACHO_RUN_FOREVER);
+	}
+	else {
+		set_tacho_speed_sp( motor[L], -ang_speed );
+		set_tacho_speed_sp( motor[R], ang_speed );
+		multi_set_tacho_command_inx(motor, TACHO_RUN_FOREVER);
+	}
+	j=0;
+	while (1) {
+		get_sensor_value0(compass_sn,&buffer[j%4]);
+		avg=0;
+		for (i = 0; i < 4; ++i) {
+			avg+=buffer[i];
+		}
+		avg=avg/4;
+		j++;
+		/*printf("COMPASS VALUE is %f\n", avg);*/
+		if (fabs(avg-dest)<=1) {
+			/*printf("FINAL COMPASS VALUE is %f\n", avg);*/
+			set_tacho_command_inx(motor[L], TACHO_STOP);
+			set_tacho_command_inx(motor[R], TACHO_STOP);
+			break;
+		}
+		else {
+			/*printf("We were so close %f\n",fabs(avg-dest));*/
+
+		}
+	}
+}
+
 
 void* tracepos()
 {
-    int rdistance, ldistance, rspeed,lspeed;
+	int rdistance, ldistance, rspeed,lspeed;
 	double R;
-	double llin_speed, rlin_speed;
-	double rpos, lpos=0;
 	R=2.7;//5,4 of diameter//centimeter
-	double pi=3.14;
-    while (1) {
-        /*get_tacho_position_sp(left_motor_sn, &ldistance);*/
-        /*printf("The left distance is %d\n",ldistance);*/
-        /*get_tacho_position_sp(right_motor_sn, &rdistance);*/
-        /*printf("The right distance is %d\n",rdistance);*/
-        get_tacho_speed(left_motor_sn, &lspeed);//tacho speed gives the degree per second
-        printf("The left speed is %d\n",lspeed);
-        get_tacho_speed(right_motor_sn, &rspeed);
-        printf("The right speed is %d\n",rspeed);
-        /*get_tacho_count_per_rot(left_motor_sn, &lspeed);*/
-        /*printf("The left deg/s is %d\n",lspeed);*/
-        /*get_tacho_count_per_rot(right_motor_sn, &rspeed);*/
-        /*printf("The right deg/s is %d\n",rspeed);*/
-		llin_speed=lspeed*R*pi/180;//speed in cm per second
-        printf("The left linear speed is %f\n",llin_speed);
-		rlin_speed=rspeed*R*pi/180;//speed in cm per second
+	while (1) {
+		/*pthread_mutex_lock(&stopMutex);*/
+		if (pos_flag) {
+			/*get_tacho_position_sp(motor[R], &ldistance);*/
+			/*printf("The left distance is %d\n",ldistance);*/
+			/*get_tacho_position_sp(motor[L], &rdistance);*/
+			/*printf("The right distance is %d\n",rdistance);*/
+			get_tacho_speed(motor[L], &lspeed);//tacho speed gives the degree per second
+			/*printf("The left speed is %d\n",lspeed);*/
+			get_tacho_speed(motor[L], &rspeed);
+			/*printf("The right speed is %d\n",rspeed);*/
+			/*get_tacho_count_per_rot(motor[L], &lspeed);*/
+			/*printf("The left deg/s is %d\n",lspeed);*/
+			/*get_tacho_count_per_rot(motor[L], &rspeed);*/
+			/*printf("The right deg/s is %d\n",rspeed);*/
+			llin_speed=lspeed*R*M_PI/180;//speed in cm per second
+			/*printf("The left linear speed is %f\n",llin_speed);*/
 
-        printf("The right linear speed is %f\n",rlin_speed);
-        Sleep(100);
-		lpos+=llin_speed*0.1;
-		printf("lpos is %f\n",lpos);
-		rpos+=rlin_speed*0.1;
-		printf("rpos is %f\n",rpos);
-    }
+			rlin_speed=rspeed*R*M_PI/180;//speed in cm per second
+			/*printf("The right linear speed is %f\n",rlin_speed);*/
+			get_sensor_value0(compass_sn, &compass_value);
+			/*printf("Compass read is %f\n",compass_value);*/
+			lpos=llin_speed*TIME_STEP/1000;
+			/*printf("lpos is %f\n",lpos);*/
+			rpos=rlin_speed*TIME_STEP/1000;
+			/*printf("rpos is %f\n",rpos);*/
+			distance=(lpos+rpos)/2;
+			/*distance=lpos;*/
+			/*distance=rpos;*/
+			angle=compass_value-ang_off;
+			/*printf("ANGLE IS %f\n",angle);*/
+			/*printf("%f %f\n",angle, distance);*/
+			coord.x+=distance*sin(angle*M_PI/180);
+			coord.y+=distance*cos(angle*M_PI/180);
+
+			/*printf("X= %f Y= %f\n",coord[0], coord[1]);*/
+			printf("%f %f\n",coord.x, coord.y);
+			Sleep(TIME_STEP);
+		}
+		else {
+			distance=0;
+			lpos=0;
+			rpos=0;
+			pthread_exit(NULL);
+		}
+	}
 }
 
-void stop(){
-    set_tacho_command_inx(left_motor_sn, TACHO_STOP);
-    set_tacho_command_inx(right_motor_sn, TACHO_STOP);
-}
-void forward(){
-    set_tacho_command_inx(left_motor_sn, TACHO_RUN_FOREVER);
-    set_tacho_command_inx(right_motor_sn, TACHO_RUN_FOREVER);
-}
+	void stop(){
+		set_tacho_command_inx(motor[L], TACHO_STOP);
+		set_tacho_command_inx(motor[R], TACHO_STOP);
+	}
+	void forward(){
+		set_tacho_speed_sp(motor[L], MAX_SPEED  / 5 );
+		set_tacho_speed_sp(motor[R], MAX_SPEED / 5 );
+		set_tacho_command_inx(motor[L], TACHO_RUN_FOREVER);
+		set_tacho_command_inx(motor[R], TACHO_RUN_FOREVER);
+	}
 
-void movement_start() {
-    movement_init();
+	void movement_start() {
+		movement_init();
 
-    // Testing. Run forward for one second.
-    /*set_tacho_command_inx(left_motor_sn, TACHO_RUN_FOREVER);*/
-    /*set_tacho_command_inx(right_motor_sn, TACHO_RUN_FOREVER);*/
-    /*Sleep(1000);*/
-    stop();
-    if(pthread_create(&movid,NULL, &tracepos, NULL)!=0){
-        printf("Failed the creation of the thread\n");
-    }
-    Sleep(1000);
+		// TESTING THE ANGLE PARAMETER= around 2.08 calibrated on 90 degrees
+		pos_flag=1;
+		/*if(pthread_create(&movid,NULL, &tracepos, NULL)!=0){*/
+			/*printf("Failed the creation of the thread\n");*/
+		/*}*/
+		/*Sleep(1000);*/
+		/*double angparam=2.10;*/
+		/*int i;*/
+		/*for (i = 0; i < 10; i+=1) {*/
+		/*printf("ANG PARAM = %f\n",angparam);*/
+		/*TURN_DEGREES(ANGULAR_SPEED, 90*angparam/2.08);*/
+		/*TURN_DEGREES_COMP(ANGULAR_SPEED*1/4, 90);*/
+		/*Sleep(10000);*/
+		/*TURN_DEGREES(ANGULAR_SPEED, -90*angparam/2.08);*/
+		/*TURN_DEGREES_COMP(ANGULAR_SPEED, 90);*/
+		/*Sleep(5000);*/
+		/*angparam-=0.05;*/
+		/*}*/
+		/*stop();*/
+		/*if(pthread_create(&movid,NULL, &tracepos, NULL)!=0){*/
+			/*printf("Failed the creation of the thread\n");*/
+		/*}*/
+		/*Sleep(1000);*/
 
 
-    mqd_t movement_queue = init_queue("/movement", O_CREAT | O_RDONLY);
-    for (int i = 0; i < 1; ++i) {
-        forward();
-        Sleep(5000);
-        stop();
-        Sleep(2000);
-    }
+		mqd_t movement_queue = init_queue("/movement", O_CREAT | O_RDONLY);
+		for (int i = 0; i < 4; ++i) {
 
-    /*while(1) {*/
-        /*uint16_t command, value;*/
-        /*get_message(movement_queue, &command, &value);*/
-        /*if (command == MESSAGE_TURN) {*/
-            /*stop();*/
-            /*set_tacho_command_inx(left_motor_sn, TACHO_RUN_FOREVER);*/
-        /*} else if (command == MESSAGE_FORWARD) {*/
-            /*forward();*/
-        /*}*/
-        /*Sleep(10);*/
-    /*}*/
-}
+			forward();
+			if(pthread_create(&movid,NULL, &tracepos, NULL)!=0){
+				printf("Failed the creation of the thread\n");
+			}
+			Sleep(2500);
+			stop();
+			Sleep(2000);
+			pos_flag=0;
+			pthread_join(movid, NULL);
+			/*printf("THESE ARE THE COORDINATES BEFORE TURNING %f %f\n",coord.x,coord.y);*/
+			/*Sleep(5000);*/
+			TURN_DEGREES_COMP(ANGULAR_SPEED/4, 90);
+			/*Sleep(5000);*/
+			/*printf("THESE ARE THE COORDINATES AFTER TURNING %f %f\n",coord.x,coord.y);*/
+			pos_flag=1;
+			Sleep(2000);
+		}
+	}
--- source/sensors.c
+++ source/sensors.c
@@ -18,13 +18,20 @@ void sensors_start(){
     ev3_search_sensor( LEGO_EV3_US, &sonar_sn, 0 );
     ev3_search_sensor( HT_NXT_COMPASS, &compass_sn, 0 );
 
-    mqd_t sensors_queue = init_queue("/sensors", O_CREAT | O_WRONLY);
 
-    while(1){
-        get_sensor_value0(compass_sn, &compass_value );
-        get_sensor_value0(sonar_sn, &sonar_value );
-        //send to main
-        send_message(sensors_queue, MESSAGE_SONAR, (uint16_t)sonar_value);
-        Sleep(100);
-    }
+	/*while (1) {*/
+		/*get_sensor_value0(compass_sn, &compass_value);*/
+		/*printf("Compass read is %f\n",compass_value);*/
+		/*Sleep(500);*/
+	/*}*/
+
+    /*mqd_t sensors_queue = init_queue("/sensors", O_CREAT | O_WRONLY);*/
+
+    /*while(1){*/
+        /*get_sensor_value0(compass_sn, &compass_value );*/
+        /*get_sensor_value0(sonar_sn, &sonar_value );*/
+        /*//send to main*/
+        /*send_message(sensors_queue, MESSAGE_SONAR, (uint16_t)sonar_value);*/
+        /*Sleep(100);*/
+    /*}*/
 }
