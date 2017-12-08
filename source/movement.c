5393b8d9ea63f305395a9e25665048fea0b506ee
mikilauda95 <michele.simili95@gmail.com>
2017-12-08 16:23:13 +0100

    gyro turn implemented

--- source/movement.c
+++ source/movement.c
@@ -27,8 +27,8 @@
 
 enum name {L, R};
 uint8_t motor[2];
-uint8_t compass_sn;
-float compass_value, ang_off;
+uint8_t gyro_sn;
+float gyro_value, ang_off;
 
 /*uint8_t motor[0];*/
 /*uint8_t motor[1];*/
@@ -62,7 +62,7 @@ int movement_init(){
 	set_tacho_speed_sp(motor[L], MAX_SPEED  / 2 );
 	set_tacho_speed_sp(motor[R], MAX_SPEED / 2 );
 
-	int rets=ev3_search_sensor( HT_NXT_COMPASS, &compass_sn, 0 );
+	int rets=ev3_search_sensor( LEGO_EV3_GYRO	, &gyro_sn, 0 );
 	if (rets) {
 		printf("FOUND IT\n");
 	}
@@ -72,7 +72,7 @@ int movement_init(){
 	Sleep(1000);
 	float a=0;
 	for (int i = 0; i < 10; ++i) {
-		get_sensor_value0(compass_sn, &ang_off);
+		get_sensor_value0(gyro_sn, &ang_off);
 		a+=ang_off;
 		Sleep(100);
 	}
@@ -94,34 +94,33 @@ static void _run_to_rel_pos( int l_speed, int l_pos, int r_speed, int r_pos )
 
 
 
-void TURN_DEGREES( int ang_speed, double angle )
-{
+void TURN_DEGREES( int ang_speed, double angle ) {
 	/*_run_to_rel_pos( ang_speed, -angle*degree_to_lin, ang_speed, angle*degree_to_lin);*/
 	_run_to_rel_pos( ang_speed, angle*degree_to_lin, ang_speed, -angle*degree_to_lin);
 	Sleep(1000);
 	/*_wait_stopped = 1;*/ //dunno what this refers to so I keep it
 }
 
-void TURN_DEGREES_COMP(int ang_speed, int angle)
+void TURN_DEGREES_GYRO(int ang_speed, int angle)
 {
 	int i=0;
 	float dest;
-	float buffer[4];
-	int j=0;
+	/*float buffer[4];*/
+	/*int j=0;*/
 	float avg=0;
-	for (i = 0; i < 4; ++i) {
-		get_sensor_value0(compass_sn,&buffer[i]);
-		/*printf("COMPASS VALUE before TURN : %f\n", buffer[i]);*/
-	}
-	for (i = 0; i < 4; ++i) {
-		avg+=buffer[i];
-	}
-	avg=avg/4;
-	dest=(avg+angle);
-	if (dest>360) {
-		dest-=360;
-	}
-	/*printf("Destination angle is %f and starting is %f\n",dest, compass_value);*/
+	/*for (i = 0; i < 4; ++i) {*/
+	get_sensor_value0(gyro_sn,&gyro_value);
+	printf("gyro VALUE before TURN : %f\n", gyro_value);
+	/*}*/
+	/*for (i = 0; i < 4; ++i) {*/
+	/*avg+=buffer[i];*/
+	/*}*/
+	/*avg=avg/4;*/
+	dest=(gyro_value+angle);
+	/*if (dest>360) {*/
+	/*dest-=360;*/
+	/*}*/
+	/*printf("Destination angle is %f and starting is %f\n",dest, gyro_value);*/
 	if (angle>0) {
 		set_tacho_speed_sp( motor[L], ang_speed );
 		set_tacho_speed_sp( motor[R], -ang_speed );
@@ -132,24 +131,39 @@ void TURN_DEGREES_COMP(int ang_speed, int angle)
 		set_tacho_speed_sp( motor[R], ang_speed );
 		multi_set_tacho_command_inx(motor, TACHO_RUN_FOREVER);
 	}
-	j=0;
+	/*j=0;*/
 	while (1) {
-		get_sensor_value0(compass_sn,&buffer[j%4]);
-		avg=0;
-		for (i = 0; i < 4; ++i) {
-			avg+=buffer[i];
-		}
-		avg=avg/4;
-		j++;
-		/*printf("COMPASS VALUE is %f\n", avg);*/
-		if (fabs(avg-dest)<=1) {
-			/*printf("FINAL COMPASS VALUE is %f\n", avg);*/
-			set_tacho_command_inx(motor[L], TACHO_STOP);
-			set_tacho_command_inx(motor[R], TACHO_STOP);
-			break;
+		get_sensor_value0(gyro_sn,&gyro_value);
+		/*avg=0;*/
+		/*for (i = 0; i < 4; ++i) {*/
+		/*avg+=buffer[i];*/
+		/*}*/
+		/*avg=avg/4;*/
+		/*j++;*/
+		printf("gyro VALUE is %f\n", gyro_value);
+		if (angle<=0) {
+			if (gyro_value<dest) {
+				printf("FINAL gyro VALUE is %f\n", gyro_value);
+				set_tacho_command_inx(motor[L], TACHO_STOP);
+				set_tacho_command_inx(motor[R], TACHO_STOP);
+				break;
+			}
+			else {
+				printf("We were so close %f\n",fabs(gyro_value-dest));
+
+			}
 		}
 		else {
-			/*printf("We were so close %f\n",fabs(avg-dest));*/
+			if (gyro_value>=dest) {
+				printf("FINAL gyro VALUE is %f\n", gyro_value);
+				set_tacho_command_inx(motor[L], TACHO_STOP);
+				set_tacho_command_inx(motor[R], TACHO_STOP);
+				break;
+			}
+			else {
+				printf("We were so close %f\n",fabs(gyro_value-dest));
+
+			}
 
 		}
 	}
@@ -181,8 +195,8 @@ void* tracepos()
 
 			rlin_speed=rspeed*R*M_PI/180;//speed in cm per second
 			/*printf("The right linear speed is %f\n",rlin_speed);*/
-			get_sensor_value0(compass_sn, &compass_value);
-			/*printf("Compass read is %f\n",compass_value);*/
+			get_sensor_value0(gyro_sn, &gyro_value);
+			/*printf("gyro read is %f\n",gyro_value);*/
 			lpos=llin_speed*TIME_STEP/1000;
 			/*printf("lpos is %f\n",lpos);*/
 			rpos=rlin_speed*TIME_STEP/1000;
@@ -190,14 +204,14 @@ void* tracepos()
 			distance=(lpos+rpos)/2;
 			/*distance=lpos;*/
 			/*distance=rpos;*/
-			angle=compass_value-ang_off;
+			angle=gyro_value-ang_off;
 			/*printf("ANGLE IS %f\n",angle);*/
 			/*printf("%f %f\n",angle, distance);*/
 			coord.x+=distance*sin(angle*M_PI/180);
 			coord.y+=distance*cos(angle*M_PI/180);
 
 			/*printf("X= %f Y= %f\n",coord[0], coord[1]);*/
-			printf("%f %f\n",coord.x, coord.y);
+			/*printf("%f %f\n",coord.x, coord.y);*/
 			Sleep(TIME_STEP);
 		}
 		else {
@@ -209,63 +223,79 @@ void* tracepos()
 	}
 }
 
-	void stop(){
-		set_tacho_command_inx(motor[L], TACHO_STOP);
-		set_tacho_command_inx(motor[R], TACHO_STOP);
-	}
-	void forward(){
-		set_tacho_speed_sp(motor[L], MAX_SPEED  / 5 );
-		set_tacho_speed_sp(motor[R], MAX_SPEED / 5 );
-		set_tacho_command_inx(motor[L], TACHO_RUN_FOREVER);
-		set_tacho_command_inx(motor[R], TACHO_RUN_FOREVER);
-	}
+void stop(){
+	set_tacho_command_inx(motor[L], TACHO_STOP);
+	set_tacho_command_inx(motor[R], TACHO_STOP);
+}
+void forward(){
+	set_tacho_speed_sp(motor[L], MAX_SPEED  / 2 );
+	set_tacho_speed_sp(motor[R], MAX_SPEED / 2 );
+	set_tacho_command_inx(motor[L], TACHO_RUN_FOREVER);
+	set_tacho_command_inx(motor[R], TACHO_RUN_FOREVER);
+}
 
-	void movement_start() {
-		movement_init();
+void movement_start() {
+	movement_init();
 
-		// TESTING THE ANGLE PARAMETER= around 2.08 calibrated on 90 degrees
-		pos_flag=1;
-		/*if(pthread_create(&movid,NULL, &tracepos, NULL)!=0){*/
-			/*printf("Failed the creation of the thread\n");*/
-		/*}*/
-		/*Sleep(1000);*/
-		/*double angparam=2.10;*/
-		/*int i;*/
-		/*for (i = 0; i < 10; i+=1) {*/
-		/*printf("ANG PARAM = %f\n",angparam);*/
-		/*TURN_DEGREES(ANGULAR_SPEED, 90*angparam/2.08);*/
-		/*TURN_DEGREES_COMP(ANGULAR_SPEED*1/4, 90);*/
-		/*Sleep(10000);*/
-		/*TURN_DEGREES(ANGULAR_SPEED, -90*angparam/2.08);*/
-		/*TURN_DEGREES_COMP(ANGULAR_SPEED, 90);*/
-		/*Sleep(5000);*/
-		/*angparam-=0.05;*/
-		/*}*/
-		/*stop();*/
-		/*if(pthread_create(&movid,NULL, &tracepos, NULL)!=0){*/
-			/*printf("Failed the creation of the thread\n");*/
-		/*}*/
-		/*Sleep(1000);*/
+	// TESTING THE ANGLE PARAMETER= around 2.08 calibrated on 90 degrees
+	pos_flag=1;
+	/*if(pthread_create(&movid,NULL, &tracepos, NULL)!=0){*/
+	/*printf("Failed the creation of the thread\n");*/
+	/*}*/
+	/*Sleep(1000);*/
+	/*double angparam=2.10;*/
+	/*int i;*/
+	/*for (i = 0; i < 10; i+=1) {*/
+	/*printf("ANG PARAM = %f\n",angparam);*/
+	/*TURN_DEGREES(ANGULAR_SPEED, 90*angparam/2.08);*/
+	/*TURN_DEGREES_GYRO(ANGULAR_SPEED*1/4, 90);*/
+	/*Sleep(10000);*/
+	/*TURN_DEGREES(ANGULAR_SPEED, -90*angparam/2.08);*/
+	/*TURN_DEGREES_GYRO(ANGULAR_SPEED, 90);*/
+	/*Sleep(5000);*/
+	/*angparam-=0.05;*/
+	/*}*/
+	/*stop();*/
+	/*if(pthread_create(&movid,NULL, &tracepos, NULL)!=0){*/
+	/*printf("Failed the creation of the thread\n");*/
+	/*}*/
+	/*Sleep(1000);*/
 
 
-		mqd_t movement_queue = init_queue("/movement", O_CREAT | O_RDONLY);
-		for (int i = 0; i < 4; ++i) {
+	mqd_t movement_queue = init_queue("/movement", O_CREAT | O_RDONLY);
+	for (int i = 0; i < 2; ++i) {
 
-			forward();
-			if(pthread_create(&movid,NULL, &tracepos, NULL)!=0){
-				printf("Failed the creation of the thread\n");
-			}
-			Sleep(2500);
-			stop();
-			Sleep(2000);
-			pos_flag=0;
-			pthread_join(movid, NULL);
-			/*printf("THESE ARE THE COORDINATES BEFORE TURNING %f %f\n",coord.x,coord.y);*/
-			/*Sleep(5000);*/
-			TURN_DEGREES_COMP(ANGULAR_SPEED/4, 90);
-			/*Sleep(5000);*/
-			/*printf("THESE ARE THE COORDINATES AFTER TURNING %f %f\n",coord.x,coord.y);*/
-			pos_flag=1;
-			Sleep(2000);
+		forward();
+		if(pthread_create(&movid,NULL, &tracepos, NULL)!=0){
+			printf("Failed the creation of the thread\n");
 		}
+		Sleep(2500);
+		stop();
+		Sleep(2000);
+		pos_flag=0;
+		pthread_join(movid, NULL);
+		/*printf("THESE ARE THE COORDINATES BEFORE TURNING %f %f\n",coord.x,coord.y);*/
+		/*Sleep(5000);*/
+		TURN_DEGREES_GYRO(ANGULAR_SPEED/2, 90);
+		/*Sleep(5000);*/
+		/*printf("THESE ARE THE COORDINATES AFTER TURNING %f %f\n",coord.x,coord.y);*/
+		pos_flag=1;
+		Sleep(2000);
+		forward();
+		if(pthread_create(&movid,NULL, &tracepos, NULL)!=0){
+			printf("Failed the creation of the thread\n");
+		}
+		Sleep(1500);
+		stop();
+		Sleep(2000);
+		pos_flag=0;
+		pthread_join(movid, NULL);
+		/*printf("THESE ARE THE COORDINATES BEFORE TURNING %f %f\n",coord.x,coord.y);*/
+		/*Sleep(5000);*/
+		TURN_DEGREES_GYRO(ANGULAR_SPEED/2, 90);
+		/*Sleep(5000);*/
+		/*printf("THESE ARE THE COORDINATES AFTER TURNING %f %f\n",coord.x,coord.y);*/
+		pos_flag=1;
+		Sleep(2000);
 	}
+}
