a2688254a5b37fb554cab44293b0a755ef51e1eb
amundas <amundas@stud.ntnu.no>
2017-12-13 20:42:22 +0100

    removed_stuff

--- source/main.c
+++ source/main.c
@@ -10,26 +10,8 @@
 #define STATE_RUNNING 2
 #define STATE_SCANNING 3
 
- mqd_t movement_queue, sensors_queue;
+mqd_t movement_queue, sensors_queue;
 
-void message_handler(uint16_t sensors_command, uint16_t sensors_value) {
-    switch (sensors_command)
-    {
-    case MESSAGE_SONAR:
-        // if the robot is close to an object, make it turn
-        if (sensors_value < 500)
-        {
-            send_message(movement_queue, MESSAGE_TURN, 0);
-            printf("Sending message TURN \n");
-        }
-        else
-        {
-            send_message(movement_queue, MESSAGE_FORWARD, 0);
-            printf("Sending message FORWARD \n");
-        }
-        break;
-    }
-}
 
 int main() {
     int state = STATE_TURNING;
@@ -38,6 +20,7 @@ int main() {
     pthread_create(&movement_thread, NULL, movement_start, NULL);
     pthread_create(&sensors_thread, NULL, sensors_start, NULL);
 
+    // To make communication bi-directional we should use two queues.
     movement_queue = init_queue("/movement", O_CREAT | O_WRONLY);
     sensors_queue = init_queue("/sensors", O_CREAT | O_RDONLY);
 
@@ -52,7 +35,7 @@ int main() {
             break;
             
             case STATE_RUNNING:
-                if (sensors_command == MESSAGE_COMPASS && sensors_value < 500) {
+                if (sensors_command == MESSAGE_SONAR && sensors_value < 500) {
                     send_message(movement_queue, MESSAGE_TURN, 10);
                     state = STATE_TURNING;
                 } 
@@ -61,7 +44,6 @@ int main() {
             case STATE_SCANNING:
             break;
         }
-        // message_handler(sensors_command, sensors_value);
         // the sleeping makes little sense now as the get_integer function blocks until it has received something.
         // This should probably be changed to nonblocking later though.
         Sleep(10);
