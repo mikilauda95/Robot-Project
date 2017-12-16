a14fc6ca2b4f510373dc55da3321e6f499858d9c
amundas <amundas@stud.ntnu.no>
2017-12-16 17:44:06 +0100

    tested_version

--- source/main.c
+++ source/main.c
@@ -97,12 +97,12 @@ void  INThandler() {
 int main() {
 
     movement_init();
-
+	
 	if (!bt_connect()) {
 		exit(1);
 	}
 	bt_wait_for_start();
-
+	
 	queue_sensors_to_main 		= init_queue("/sensors", O_CREAT | O_RDWR | O_NONBLOCK);
 	queue_main_to_move 			= init_queue("/movement_from_main", O_CREAT | O_RDWR);
 	queue_move_to_main 			= init_queue("/movement_to_main", O_CREAT | O_RDWR | O_NONBLOCK);
