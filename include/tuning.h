d09f2037ea8ba7442ac8e43d7f8a6231a2095b2b
amundas <amundas@stud.ntnu.no>
2018-01-12 13:01:23 +0100

    fixed typo

--- include/tuning.h
+++ include/tuning.h
@@ -1,6 +1,6 @@
 #define ROBOT_START_X 400 // Robot start position in mm
 #define ROBOT_START_Y 100
-#define START_HEADING 90 // Robot star heading in degrees
+#define START_HEADING 90 // Robot start heading in degrees
 #define MAX_SCAN_DIST 500 // Max distance in mm
 
 #define FORWARD_SPEED 350 // Wheel speed when running forward. Max is 1050
